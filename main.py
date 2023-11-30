2ee98d1f729ab78e842483a3e497fb764016fd89
meltland2002 <147283275+meltland2002@users.noreply.github.com>
2023-11-30 14:43:13 -0500

    add pinging

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import websocket
 import json
 from notifypy import Notify
+import threading
+import time
 
 def on_message(ws, message):
     # print(message)
@@ -33,7 +35,14 @@ def on_close(ws, close_status_code, close_msg):
    notif.icon = "meower.png"
    notif.send()
    print("Lost connection!")
+   
+def ping(ws):
+    print("Pinger active!")
+    time.sleep(7.5)
+    ws.send(json.dumps({"cmd": "ping", "val": ""}))
     
 if __name__ == "__main__":
     ws = websocket.WebSocketApp("wss://server.meower.org/", on_message=on_message, on_close=on_close)
+    pinger = threading.Thread(target=ping, args=(ws,), daemon=True)
+    pinger.start()
     ws.run_forever()
